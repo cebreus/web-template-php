33c397482dfbcdd45739acb8524d425cf2d2819b
cebreus <cebreus69@gmail.com>
2017-07-16 17:15:06 +0200

    Refactoring index.css.php

--- skins/default/index.css.php
+++ skins/default/index.css.php
@@ -10,12 +10,10 @@ require_once '../../vendor/helpers/scss-helpers.php';
 use Leafo\ScssPhp\Compiler;
 
 $buildDir = '../../build/css';
-$cssTempDir = '../../.tmp';
+$cssTempDir = '../../.tmp/css';
 $sourceDir = '.';
 $scss = new Compiler();
 $scss->setImportPaths($sourceDir);
-
-isProduction($GLOBALS['siteDomain']) ? $scss->setFormatter('Leafo\ScssPhp\Formatter\Crunched') : $scss->setFormatter('Leafo\ScssPhp\Formatter\Expanded');
 
 !file_exists($buildDir) ? mkdir($buildDir, 0777, true) : null;
 !file_exists($cssTempDir) ? mkdir($cssTempDir, 0777, true) : null;
@@ -25,28 +23,46 @@ isProduction($GLOBALS['siteDomain']) ? $scss->setFormatter('Leafo\ScssPhp\Format
 // --------------
 
 ob_start();
+$scss->setFormatter('Leafo\ScssPhp\Formatter\Crunched');
+\SCSSHelpers\buildCSSFromFile($sourceDir, $cssTempDir, $scss, '_show-grid.scss');
+$showGrid = ob_get_contents();
+ob_end_clean();
 
-isProduction($GLOBALS['siteDomain']) ? null : \SCSSHelpers\buildCSSFromFile($sourceDir, $cssTempDir, $scss, 'show-breakpoints.scss');
-
-$fonts  = '@font-face {font-family: "Open Sans"; font-style: normal; font-weight: 300; src: local("Open Sans Light"), local("OpenSans-Light"), url("/build/fonts/open-sans-300-latin-ext.woff") format("woff");}';
-$fonts .= '@font-face {font-family: "Material Icons"; font-style: normal; font-weight: 400; src: local("Material Icons"), local("MaterialIcons-Regular"), url("/build/fonts/material-icons.woff") format("woff");}';
-$fonts .= '.material-icons {font-family: "Material Icons"; font-weight: normal; font-style: normal; font-size: 24px; line-height: 1; letter-spacing: normal; text-transform: none; display: inline-block; white-space: nowrap; word-wrap: normal; direction: ltr; font-feature-settings: "liga";}';
-$fonts = \SCSSHelpers\getStringFromCompiler($fonts, $scss, TRUE);
-
-echo $fonts . PHP_EOL . PHP_EOL;
-
+ob_start();
+$scss->setFormatter('Leafo\ScssPhp\Formatter\Expanded');
+\SCSSHelpers\buildCSSFromFile($sourceDir, $cssTempDir, $scss, 'fonts/fonts.scss');
 \SCSSHelpers\buildCSSFromFile($sourceDir, $cssTempDir, $scss, 'bootstrap.scss');
 \SCSSHelpers\buildCSSFromFile($sourceDir, $cssTempDir, $scss, 'custom.scss');
-
 $fileGeneratedContent = ob_get_contents();
+ob_end_clean();
 
-ob_end_flush();
+ob_start();
+$scss->setFormatter('Leafo\ScssPhp\Formatter\Crunched');
+\SCSSHelpers\buildCSSFromFile($sourceDir, $cssTempDir.'-crunched', $scss, 'fonts/fonts.scss');
+\SCSSHelpers\buildCSSFromFile($sourceDir, $cssTempDir.'-crunched', $scss, 'bootstrap.scss');
+\SCSSHelpers\buildCSSFromFile($sourceDir, $cssTempDir.'-crunched', $scss, 'custom.scss');
+$fileContentSanitized = ob_get_contents();
+ob_end_clean();
 
 
-// Generate static file
+// Sanitize content
 // --------------
 
-$fileGeneratedContent = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/ !', PHP_EOL.PHP_EOL, $fileGeneratedContent);
-$fileGeneratedContent = trim($fileGeneratedContent);
+$fileContentSanitized = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', PHP_EOL.PHP_EOL, $fileContentSanitized);
+$fileContentSanitized = preg_replace('/ -[ \n\r\t]+/', ' - ', $fileContentSanitized);
+$fileContentSanitized = preg_replace('/[\n\r\t]+/', PHP_EOL, $fileContentSanitized);
+$fileContentSanitized = preg_replace('/[ ]+/', ' ', $fileContentSanitized);
+$fileContentSanitized = trim($fileContentSanitized);
 
-writeFile($buildDir.'/', 'static.min.css', $fileGeneratedContent);
+
+// Write file
+// --------------
+
+writeFile($buildDir.'/static.min.css', $fileContentSanitized);
+
+
+// Show result
+// --------------
+
+echo $fileGeneratedContent;
+echo isProduction($GLOBALS['siteDomain']) ? '' : $showGrid;
